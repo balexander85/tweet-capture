c78b6f9cf26437a134d5daad160a5f17be0674f6
Brian Alexander <balexander04@gmail.com>
2020-05-03 20:55:31 -0500

    Trying to implement pathlib

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from configparser import ConfigParser
+from pathlib import Path
 import os
 
 PROJECT_DIR_PATH = os.path.dirname(__file__)
+CONFIG_PATH = str(Path.cwd().joinpath("conf", "config.ini"))
 
 config = ConfigParser()
-config.read(os.path.join(PROJECT_DIR_PATH, "conf", "config.ini"))
+config.read(CONFIG_PATH)
 
 # Selenium config
 CHROME_DRIVER_PATH = config.get("default", "CHROME_DRIVER_PATH")
