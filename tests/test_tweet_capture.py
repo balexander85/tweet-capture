0b5b344e02a38dd5ec80b59144190b05a660a2d5
Brian Alexander <balexander04@gmail.com>
2021-11-24 21:16:26 -0600

    Updating import path

--- tests/test_tweet_capture.py
+++ tests/test_tweet_capture.py
@@ -4,7 +4,7 @@ from os import path
 from furl import furl
 import pytest
 
-from src.tweet_capture import (
+from tweet_capture import (
     TweetCapture,
     dismiss_sensitive_material_warning,
 )
